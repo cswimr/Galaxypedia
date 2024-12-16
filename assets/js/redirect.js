a68f2748f6a717d344ff3ad9ffd2609f2365b8d4
cswimr <seaswimmerthefsh@gmail.com>
2024-12-16 14:06:09 -0500

    add redirect.js and add it to baseof
    this script automatically redirects to `.Site.BaseURL` if the request comes from another domain

---
+++ assets/js/redirect.js
@@ -0,0 +1,8 @@
+(function () {
+  const baseUrl = "{{ .Site.BaseURL | safeJS }}".replace(/\/$/, "");
+  const url = new URL(window.location.href);
+  if (url.pathname.startsWith(baseUrl)) {
+    const newUrl = new URL(url.pathname.replace(baseUrl, ""), baseUrl);
+    window.location.href = newUrl.toString();
+  }
+});
