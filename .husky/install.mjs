8e8cafe19af7de4485edbdf833b19824b1cb98b1
cswimr <seaswimmerthefsh@gmail.com>
2024-12-17 06:43:08 -0500

    fix husky trying to install itself in CI and failing

---
+++ .husky/install.mjs
@@ -0,0 +1,7 @@
+// Skip Husky install in CI
+// https://typicode.github.io/husky/how-to.html#ci-server-and-docker
+if (process.env.CI === 'true') {
+    process.exit(0)
+  }
+  const husky = (await import('husky')).default
+  console.log(husky())
